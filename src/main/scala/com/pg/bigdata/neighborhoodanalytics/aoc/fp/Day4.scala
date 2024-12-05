acf851959dac1f77a5b526498b3735eaa89ad25e
Path-Finder-00 <lukasz_golab_vib@wp.pl>
2024-12-06 00:21:38 +0100

    A little more readable

--- src/main/scala/com/pg/bigdata/neighborhoodanalytics/aoc/fp/Day4.scala
+++ src/main/scala/com/pg/bigdata/neighborhoodanalytics/aoc/fp/Day4.scala
@@ -11,33 +11,22 @@ object Day4 extends Exercise(2024, 4) {
   private val mas = "MAS"
   private val sam = "SAM"
 
+  private type Matrix = List[List[Char]]
+  private type MatricesPerXLines = List[Matrix]
+  private type Matrices = List[MatricesPerXLines]
+
   private def makeXLiners(input: List[String], lines: Int): List[List[String]] = {
     input.sliding(lines).toList
   }
 
-  private def makeXxXMatrices(fourLiner: List[String], size: Int): List[List[List[Char]]] = {
-      fourLiner.transpose.sliding(size).toList
+  private def makeXxXMatrices(xLiner: List[String], size: Int): MatricesPerXLines = {
+      xLiner.transpose.sliding(size).toList
   }
 
-//  val fourLiners = make4Liners(List("MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM", "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"))
-//
-//  val listOfMatrices = fourLiners.map { fourLiner => make4x4Matrices(fourLiner) }
-//  val totals = findTotal(listOfMatrices)
-//  print(totals)
-
-//  println(
-//    make4x4Matrices(fourLiners.head).map { matrix => {
-//      val transposed = matrix.transpose
-//      findHorizontal(transposed, true) + findVertical(transposed, true) + findDiagonal(transposed)
-//    }}.sum)
-//  println(fourLiners.tail.map(fourLiner => { make4x4Matrices(fourLiner) }).flatMap { matrices => matrices.map { matrix => { val transposed = matrix.transpose; findHorizontal(transposed, false) + findVertical(transposed, false) + findDiagonal(transposed) } } }.sum)
-
-//  fourLiners.map(fourLiner => { make4x4Matrices(fourLiner) }).foreach { matrices => matrices.foreach { matrix => { matrix.transpose.foreach(println); println("VERTICAL: " + findVertical(matrix, false)); println("HORIZONTAL: " + findHorizontal(matrix, false)); println("DIAGONAL: " + findDiagonal(matrix)); println("----") } } }
-
-  private def findHorizontal(matrix: List[List[Char]], firstFour: Boolean): Int = {
+  private def findHorizontal(matrix: Matrix, firstFour: Boolean): Int = {
 
     @tailrec
-    def helper(matrix: List[List[Char]], acc: Int): Int = matrix match {
+    def helper(matrix: Matrix, acc: Int): Int = matrix match {
       case Nil => acc
       case row :: tail => if (row.mkString("") == xmas || row.mkString("") == samx) helper(tail, acc + 1) else helper(tail, acc)
     }
@@ -49,10 +38,10 @@ object Day4 extends Exercise(2024, 4) {
     }
   }
 
-  private def findVertical(matrix: List[List[Char]], firstColumn: Boolean): Int = {
+  private def findVertical(matrix: Matrix, firstColumn: Boolean): Int = {
 
     @tailrec
-    def helper(matrix: List[List[Char]], column: Int, acc: Int): Int = column match {
+    def helper(matrix: Matrix, column: Int, acc: Int): Int = column match {
       case 4 => acc
       case _ => List(matrix(0)(column), matrix(1)(column), matrix(2)(column), matrix(3)(column)).mkString("") match {
        case `xmas` | `samx` => helper(matrix, column + 1, acc + 1)
@@ -67,7 +56,7 @@ object Day4 extends Exercise(2024, 4) {
     }
   }
 
-  private def findDiagonal(matrix: List[List[Char]]): Int = {
+  private def findDiagonal(matrix: Matrix): Int = {
     val diagonal1 = List(matrix(0)(0), matrix(1)(1), matrix(2)(2), matrix(3)(3)).mkString("")
     val diagonal2 = List(matrix(0)(3), matrix(1)(2), matrix(2)(1), matrix(3)(0)).mkString("")
 
@@ -76,10 +65,10 @@ object Day4 extends Exercise(2024, 4) {
     firstDiagonal + secondDiagonal
   }
 
-  private def findTotalForEachCase(matricesPerEachFourLines: List[List[List[Char]]], firstFour: Boolean): Int = {
+  private def findTotalForEachCase(matricesPerFourLines: MatricesPerXLines, firstFour: Boolean): Int = {
 
     @tailrec
-    def helper(matrices: List[List[List[Char]]], acc: Int, firstColumn: Boolean): Int = matrices match {
+    def helper(matrices: MatricesPerXLines, acc: Int, firstColumn: Boolean): Int = matrices match {
       case Nil => acc
       case matrix :: tail =>
         val transposed = matrix.transpose
@@ -89,10 +78,10 @@ object Day4 extends Exercise(2024, 4) {
         helper(tail, acc + horizontal + vertical + diagonal, false)
     }
 
-    helper(matricesPerEachFourLines, 0, true)
+    helper(matricesPerFourLines, 0, true)
   }
 
-  private def findTotal(matricesPerEachFourLines: List[List[List[List[Char]]]]): Int = {
+  private def findTotal(matricesPerEachFourLines: Matrices): Int = {
     val firstFour = matricesPerEachFourLines.head
     val totalForFirstFour = findTotalForEachCase(firstFour, true)
     val totalForTheRest = matricesPerEachFourLines.tail.map { matrices => findTotalForEachCase(matrices, false) }.sum
@@ -108,7 +97,7 @@ object Day4 extends Exercise(2024, 4) {
     } yield totals.toString
   }
 
-  private def findBothDiagonals(matrix: List[List[Char]]): Int = {
+  private def findBothDiagonals(matrix: Matrix): Int = {
     val diagonal1 = List(matrix(0)(0), matrix(1)(1), matrix(2)(2)).mkString("")
     val diagonal2 = List(matrix(0)(2), matrix(1)(1), matrix(2)(0)).mkString("")
 
@@ -117,10 +106,10 @@ object Day4 extends Exercise(2024, 4) {
     if (firstDiagonal + secondDiagonal == 2) 1 else 0
   }
 
-  private def findTotalXs(matricesPerEachThreeLines: List[List[List[Char]]]): Int = {
+  private def findTotalXs(matricesPerEachThreeLines: MatricesPerXLines): Int = {
 
     @tailrec
-    def helper(matrices: List[List[List[Char]]], acc: Int): Int = matrices match {
+    def helper(matrices: MatricesPerXLines, acc: Int): Int = matrices match {
       case Nil => acc
       case matrix :: tail =>
         val diagonal = findBothDiagonals(matrix)
@@ -138,4 +127,5 @@ object Day4 extends Exercise(2024, 4) {
       _ = println(totals)
     } yield totals.toString
   }
+
 }
